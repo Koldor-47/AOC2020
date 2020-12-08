fdd181c2a7f016ab3881340f6bcf4ab6d6b63fd5
koldor <nickhowlett13@gmail.com>
2020-12-08 19:12:47 +1030

    Updating Puzzle Day5

---
+++ Advent2020/Day5/Day5.py
@@ -0,0 +1,45 @@
+
+
+def get_input_data(input_data):
+    with open(input_data, 'r') as F:
+        thedata = F.read().split("\n")
+    return thedata
+
+def data_to_number(row_data, letters):
+    # letters needs to a list ie ["F", "B"]
+    answer = "0b"
+    for pos in row_data:
+        if pos.upper() == letters[1]:
+            answer += "1"
+        elif pos.upper() == letters[0]:
+            answer += "0"
+        else:
+            print("invalid")
+
+    return int(answer, 2)
+
+
+def get_seat_id(bin_data, codes):
+    # Codes are all 4 toe of letters in 1 list ["F","B","L","R"]
+    rows = codes[:2]
+    cols = codes[2:]
+    seat_id_list = []
+    for ticket in bin_data:
+        part2 = data_to_number(ticket[:-3], rows)
+        part1 = data_to_number(ticket[-3:], cols)
+        seat_id = (part2 * 8) + part1
+        seat_id_list.append(seat_id)
+
+    return seat_id_list
+
+
+# Single Test
+Seat_number = "BBFFBBFRLL"
+answer = data_to_number(Seat_number[:-3], ["F", "B"])
+print(answer)
+
+#Answer Part 1
+puzzle1 = get_seat_id(get_input_data("Day5Data.txt"), ["F","B","L","R"])
+
+print(max(puzzle1))
+
